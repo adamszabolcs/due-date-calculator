10644032eee1bbf594aca1a8152130c804d90a08
adamszabolcs <szabolcs.adam91@gmail.com>
2022-01-21 09:54:09 +0100

    created test class for DueDateCalculator class, implemented smoke test and first constructor test

---
+++ src/test/java/com/emarsys/calculator/DueDateCalculatorTest.java
@@ -0,0 +1,23 @@
+package com.emarsys.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DueDateCalculatorTest {
+
+    private DueDateCalculator calculator = new DueDateCalculator(LocalTime.of(9, 0), LocalTime.of(17, 0));
+
+    @Test
+    void smoke() {
+        assertTrue(true);
+    }
+
+    @Test
+    void constructorShouldThrowExceptionIfStartHourIsAfterEndHour() {
+        assertThrows(IllegalArgumentException.class, () -> new DueDateCalculator(LocalTime.of(12, 0), LocalTime.of(9, 0)));
+    }
+
+}
